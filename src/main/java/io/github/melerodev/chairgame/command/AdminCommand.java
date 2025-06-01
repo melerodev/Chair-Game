cc68ec7bde04ac7b15eaeadac47a8a40199e80aa
Alejandro Melero <80626286+melerodev@users.noreply.github.com>
2025-06-01 19:17:50 +0200

    Renamed ArenasCommand to ArenaSubcommandBuilder and updated AdminCommand to use the new subcommand builder for improved command structure

--- src/main/java/io/github/melerodev/chairgame/command/AdminCommand.java
+++ src/main/java/io/github/melerodev/chairgame/command/AdminCommand.java
@@ -2,14 +2,9 @@ package io.github.melerodev.chairgame.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.executors.CommandArguments;
-import io.github.melerodev.chairgame.ChairGame;
-import io.github.melerodev.chairgame.arena.Arena;
 import io.github.melerodev.chairgame.permission.Permissions;
 import io.github.milkdrinkers.wordweaver.Translation;
 import org.bukkit.command.CommandSender;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AdminCommand {
     protected CommandAPICommand command() {
@@ -19,7 +14,7 @@ public class AdminCommand {
             .withShortDescription("ChairGame command.")
             .withPermission(Permissions.ADMIN_PERMISSION.getNode())
             .withSubcommands(
-                ArenasCommand.buildArenaSubcommands().toArray(new CommandAPICommand[0])
+                ArenaSubcommandBuilder.buildArenaSubcommands().toArray(new CommandAPICommand[0])
             )
             .executes(this::executorAdmin);
     }
--- src/main/java/io/github/melerodev/chairgame/command/ArenaSubcommandBuilder.java
+++ src/main/java/io/github/melerodev/chairgame/command/ArenaSubcommandBuilder.java
@@ -3,12 +3,11 @@ package io.github.melerodev.chairgame.command;
 import dev.jorel.commandapi.CommandAPICommand;
 import io.github.melerodev.chairgame.ChairGame;
 import io.github.melerodev.chairgame.arena.Arena;
-import io.github.melerodev.chairgame.permission.Permissions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArenasCommand {
+public class ArenaSubcommandBuilder {
     public static List<CommandAPICommand> buildArenaSubcommands() {
         List<CommandAPICommand> arenaSubcommands = new ArrayList<>();
 
